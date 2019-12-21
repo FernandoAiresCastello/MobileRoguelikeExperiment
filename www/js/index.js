c8f02abc489b165b3336027f85ec1d9cddbb3998
Fernando <fernandoairescastello@gmail.com>
2019-12-20 21:38:38 -0300

    Mostly UI updates

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,90 @@
 
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "complete") {
+        Main();
+    }
+});
+
+let Buttons;
+let ButtonActiveColor = '#555'
+let ButtonInactiveColor = '#333'
+let ButtonA;
+let ButtonB;
+
 const Cols = 11;
 const Rows = 11;
 const SmileyCount = 100;
 
 const Smiley = 
-    '01111110'+
-    '11111111'+
-    '11011011'+
-    '11111111'+
-    '11111111'+
-    '11011011'+
-    '11100111'+
-    '01111110';
+    '01111100'+
+    '11111010'+
+    '11111110'+
+    '11111010'+
+    '11111010'+
+    '11110110'+
+    '01111100'+
+    '00000000';
 
-document.addEventListener('readystatechange', event => {
-    if (event.target.readyState === "complete") {
-        main();
-    }
-});
+function Main() {
 
-function random(min, max) {
-    return Math.floor(Math.random() * max) + min;
-}
-
-function main() {
-
+    InitButtons();
     InitGraphics(Cols, Rows, 2);
-    putRandomSmileys();
-
-    let button = document.getElementById('star');
-    button.addEventListener('click', () => {
-        putRandomSmileys();
-    });
+    PutRandomSmileys();
+    Print('This is a test...');
 }
 
-function putRandomSmileys() {
+function InitButtons() {
+
+    Buttons = document.getElementsByTagName('button');
+
+    for (let button of Buttons) {
+        button.style.background = ButtonInactiveColor;
+
+        button.onpointerenter = (event) => {
+            button.pressed = true;
+            button.style.background = ButtonActiveColor;
+        };
+        button.onpointerleave = (event) => {
+            button.pressed = false;
+            button.style.background = ButtonInactiveColor;
+        };
+    }
+
+    ButtonA = document.getElementById('button-a');
+    ButtonB = document.getElementById('button-a');
+
+    setInterval(() => {
+        if (ButtonA.pressed) {
+            PutRandomSmileys();
+        }
+    }, 10);
+}
+
+function log(text) {
+    console.log(text);
+}
+
+function Print(text) {
+    const TextArea = document.getElementById('scrolling-text');
+    TextArea.innerText = text;
+}
+
+function PutRandomSmileys() {
     
     Cls();
 
     for (var i = 0; i < SmileyCount; i++) {
 
-        const x = random(0, Cols);
-        const y = random(0, Rows);
-        const color = random(0, Palette.length);
+        const x = Random(0, Cols);
+        const y = Random(0, Rows);
+        const color = Random(0, Palette.length);
 
         DrawTile(x, y, Palette[color], BackColor, Smiley);
     }
 
     //DrawGrid();
+}
+
+function Random(min, max) {
+    return Math.floor(Math.random() * max) + min;
 }
